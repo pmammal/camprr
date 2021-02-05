c891cfedd47d7d35dacca7006f33f7d4ca7f42ae
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-05 13:01:32 -0600

    configure passport

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 //DATABASE CONNECTION************************************
 mongoose.connect('mongodb://localhost:27017/camprr', {
@@ -35,6 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//SESSION************************************************
 const sessionConfig = {
 	secret            : 'thisshouldbeabettersecret!',
 	resave            : false,
@@ -45,8 +49,16 @@ const sessionConfig = {
 		maxAge   : 1000 * 60 * 60 * 24 * 7
 	}
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
