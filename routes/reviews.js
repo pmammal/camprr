747d65cc6453da311d95d6d23a7c7467aa69b090
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-08 13:11:59 -0600

    validation for deleting reviews and visibility of btns

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,14 +5,17 @@ const Review = require('../models/review');
 const { reviewSchema } = require('../schemas.js');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+
 //REVIEWS
 router.post(
 	'/',
+	isLoggedIn,
 	validateReview,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id);
 		const review = new Review(req.body.review);
+		review.author = req.user._id;
 		campground.reviews.push(review);
 		await review.save();
 		await campground.save();
@@ -23,6 +26,8 @@ router.post(
 
 router.delete(
 	'/:reviewId',
+	isLoggedIn,
+	isReviewAuthor,
 	catchAsync(async (req, res) => {
 		const { id, reviewId } = req.params;
 		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
