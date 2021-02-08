b2b414cebe3ab5c47bbd0afc8e2926a2ed409f52
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-08 12:45:01 -0600

    restructure middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,17 +5,7 @@ const Review = require('../models/review');
 const { reviewSchema } = require('../schemas.js');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map((el) => el.message).join(',');
-		throw new ExpressError(msg, 400);
-	} else {
-		next();
-	}
-};
-
+const { validateReview } = require('../middleware');
 //REVIEWS
 router.post(
 	'/',
