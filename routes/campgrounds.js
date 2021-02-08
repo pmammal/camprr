cf9e90e69e831592ebb7c824346470624b09e308
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-08 16:07:35 -0600

    update form for multiple image uploads

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const Campground = require('../models/campground');
 
@@ -10,8 +12,9 @@ router
 	.route('/')
 	.get(catchAsync(campgrounds.index))
 	// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-	.post((req, res) => {
-		res.send(req.body);
+	.post(upload.array('image'), (req, res) => {
+		console.log(req.body, req.files);
+		res.send('ok');
 	});
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
