46fbcde9d59d400f5943f80a45fb836f6a72800f
Elliott Wilder <elliottjwilder@gmail.com>
2021-02-09 13:54:30 -0600

    change image display on edit page to thumbnails

--- models/campground.js
+++ models/campground.js
@@ -1,14 +1,21 @@
+const { string } = require('joi');
 const mongoose = require('mongoose');
 const review = require('./review');
 const Schema = mongoose.Schema;
 
+
+const ImageSchema = new Schema({
+	url: String,
+	filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function() {
+	return this.url.replace('/upload', '/upload/w_200')
+})
 const CampgroundSchema = new Schema({
 	title       : String,
 	images      : [
-		{
-			url      : String,
-			filename : String
-		}
+		ImageSchema
 	],
 	price       : Number,
 	description : String,
@@ -24,6 +31,8 @@ const CampgroundSchema = new Schema({
 		}
 	]
 });
+
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
 	if (doc) {
